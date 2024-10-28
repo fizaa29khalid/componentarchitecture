52e43e65e0dcad6537a4673dd03899dafadfeba8
fizaa29khalid <165763070+fizaa29khalid@users.noreply.github.com>
2024-10-28 01:32:31 -0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,7 +1,7 @@
-export default function ServicePage() {
+export default function CoderPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Our Services</h1>
+      <h1 className="text-4xl font-bold text-blue-600">Coder Jobs</h1>
     </div>
   );
 }
